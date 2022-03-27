bf6d899059aaf72a9ab9378963d2be0a3ad07241
Orunto Eniola <oruntoeniola@aol.com>
2022-03-27 15:08:17 +0100

    Save Progress

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ function Button (props) {
     );
 }
 
-function Cards () {
+function Cards (props) {
     return(
-        <div className="Cards"></div>
+        <div className="Cards">{props.children}</div>
     );
 }
 
@@ -48,12 +48,31 @@ function Simple () {
     return (
         <section className="Simple">
             <h2>Fashion should not be difficult and we make it even simpler</h2>
-            <div id="info"><Cards></Cards></div>
-            <div id="stores"><Cards></Cards></div>
-            <div id="middle"><Cards></Cards></div>
-            <div id="connect"><Cards></Cards></div>
-            <div id="auto"><Cards></Cards></div>
-
+            <div id="info">
+                <Cards>
+                    <p>Find the information you need, answers to any questions</p>
+                </Cards>
+            </div>
+            <div id="stores">
+                <Cards>
+                <p>Find the best stores to shop from</p>
+                </Cards>
+            </div>
+            <div id="middle">
+                <Cards>
+                <p>Use our AI to build up your styles</p>
+                </Cards>
+            </div>
+            <div id="connect">
+                <Cards>
+                <p>Connect to the best of fashion talent in Nigeria</p>
+                </Cards>
+            </div>
+            <div id="auto">
+                <Cards>
+                <p>Simple, automated wardrobe building plans.</p>
+                </Cards>
+            </div>
             <Button>
                 <span>Get Started</span>
             </Button>
