81cc1a51985350f64a76755242f40e72cea80609
Orunto Eniola <oruntoeniola@aol.com>
2022-03-27 13:13:04 +0100

    Upload all files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function Header (){
+
+}
+
+function ForMen() {
+
+}
+
+function Simple () {
+
+}
+
+function Dressed() {
+
+}
+
+function Socials() {
+
+}
+function Home (){
+
+}
+
+ReactDOM.render(<Home />, document.getElementById("root"));
