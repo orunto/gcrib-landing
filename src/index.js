e428957b1e7f83492014b6e93d36e3528b93e363
Orunto Eniola <oruntoeniola@aol.com>
2022-05-09 23:14:35 +0100

    Social buttons

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ function Simple () {
             </div>
             <div id="stores">
                 <Cards>
-                <img src="/images/search.svg"></img>
+                <img src="/images/Location.svg"></img>
                 <p>Find the best stores to shop from</p>
                 </Cards>
             </div>
@@ -133,11 +133,29 @@ function Socials (){
         <section id="Socials">
             <h2>We like to talk a lot on social media about fashion</h2>
             <div className="Spread">
-                <div id="insta"><a href="https://www.instagram.com/gentlemens_crib_ng/"><TalkButton><img src="./images/Instagram.svg"/></TalkButton></a></div>
-                <div id="facebook"><a href="https://www.facebook.com/pages/category/E-commerce-Website/Gentlemens-Crib-101456768779286/"><TalkButton><img src="./images/Facebook.svg"/></TalkButton></a></div>
-                <div id="twitter"><a href="https://twitter.com/gentlemenscrib"><TalkButton><img src="./images/Twitter.svg"/></TalkButton></a></div>
-                <div id="whatsapp"><a href="https://wa.me/2348027132741"><TalkButton><img src="./images/Whatsapp.svg"/></TalkButton></a></div>
-                <div id="linkedin"><a href="https://linkedin.com/"><TalkButton><img src="./images/LinkedIn.svg"/></TalkButton></a></div>
+                <div id="insta"><a href="https://www.instagram.com/gentlemens_crib_ng/">
+                    <TalkButton>
+                        <img src="./images/Instagram.svg"/>
+                        <p>@gentlemans_crib_ng</p>
+                        <span>Visit Page</span>
+                    </TalkButton></a>
+                </div>
+                <div id="facebook"><a href="https://www.facebook.com/pages/category/E-commerce-Website/Gentlemens-Crib-101456768779286/"><TalkButton><img src="./images/Facebook.svg"/>
+                <p>Gentlemen's Crib</p>
+                        <span>Visit Page</span>
+                </TalkButton></a></div>
+                <div id="twitter"><a href="https://twitter.com/gentlemenscrib"><TalkButton><img src="./images/Twitter.svg"/>
+                <p>@GentlemensCrib</p>
+                        <span>Visit Page</span>
+                </TalkButton></a></div>
+                <div id="whatsapp"><a href="https://wa.me/2348027132741"><TalkButton><img src="./images/Whatsapp.svg"/>
+                <p>wa.me/2348027132741</p>
+                        <span>Send a Text</span>
+                </TalkButton></a></div>
+                <div id="linkedin"><a href="https://linkedin.com/"><TalkButton><img src="./images/LinkedIn.svg"/>
+                <p>Gentlemen's Crib</p>
+                        <span>Visit Profile</span>
+                </TalkButton></a></div>
             </div>
         </section>
     );
