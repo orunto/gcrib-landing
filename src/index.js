3728ac5ddba6e8c6cccebb019479bd5546f04cfc
Orunto Eniola <oruntoeniola@aol.com>
2022-05-09 23:02:27 +0100

    Icons

--- src/index.js
+++ src/index.js
@@ -80,26 +80,31 @@ function Simple () {
             <p className="simpler">...and we make it even simpler</p>
             <div id="info">
                 <Cards>
+                    <img src="/images/search.svg"></img>
                     <p>Find the information you need, answers to any questions</p>
                 </Cards>
             </div>
             <div id="stores">
                 <Cards>
+                <img src="/images/search.svg"></img>
                 <p>Find the best stores to shop from</p>
                 </Cards>
             </div>
             <div id="middle">
                 <Cards>
+                <img src="/images/AI.svg"></img>
                 <p>Use our AI to build up your styles</p>
                 </Cards>
             </div>
             <div id="connect">
                 <Cards>
+                <img src="/images/Needle.svg"></img>
                 <p>Connect to the best of fashion talent in Nigeria</p>
                 </Cards>
             </div>
             <div id="auto">
                 <Cards>
+                <img src="/images/Hanger.svg"></img>
                 <p>Simple, automated wardrobe building plans.</p>
                 </Cards>
             </div>
